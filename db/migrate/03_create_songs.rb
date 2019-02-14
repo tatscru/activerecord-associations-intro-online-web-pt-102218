ea9923c9d31b4078bcf16471d90cf1f4fd06267a
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-14 03:24:34 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,1 +1,9 @@
-class 
+class CreateSongs < ActiveRecord::Migration[4.2]
+  def change 
+    create_table :songs do |t|
+      t.string :name 
+      t.integer :artist_id
+      t.integer :genre_id
+    end 
+  end
+end 
